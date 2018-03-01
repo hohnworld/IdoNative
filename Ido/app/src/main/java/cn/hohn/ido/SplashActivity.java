ddb31e43ec156f78b3194f814b4c39d8d4538707
hohnworld <292920487@qq.com>
2018-03-01 09:53:08 +0800

    logosplash

---
+++ Ido/app/src/main/java/cn/hohn/ido/SplashActivity.java
@@ -0,0 +1,29 @@
+package cn.hohn.ido;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Bundle;
+import android.view.View;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        },2000);
+
+
+    }
+}
